5e8b4db2738595e97e54c9549e7946d45a23901a
WafaaAlAiban <76471019+WafaaAlAiban@users.noreply.github.com>
2021-01-04 18:13:25 +0300

    Done

--- question1/question1.js
+++ question1/question1.js
@@ -1,1 +1,12 @@
 // 👇🏻 قم بحل السؤال الأول هنا
+let Name = prompt(`Hello, my name is Wafaa. What is your name?`)
+function sayHello(){
+    console.log(Name)
+}
+sayHello();
+if (Name==``){
+    alert(`Hello`)
+}
+else{
+    alert(`Hello ${Name}`)
+}
